2cc9b2da852c39248eeae41597962173ed718adf
shalinis2023-siva <shalini.s2023@vitstudent.ac.in>
2026-03-28 16:57:58 +0530

    Added Fibonacci logic

---
+++ fibonacci.py
@@ -0,0 +1,10 @@
+# Fibonacci Program
+
+def fibonacci(n):
+    a, b = 0, 1
+    for i in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+
+n = int(input("Enter number of terms: "))
+fibonacci(n)
